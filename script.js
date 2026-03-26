020d54a1ebeaad59dfd302fa00edead3d1db93b9
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-03-26 06:07:47 -0500

    Add javascript code to toggle password visibility for both password and confirm password fields in the sign-up form.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const togglePassword = document.querySelector('#togglePassword');
+const togglePassword1 = document.querySelector('#togglePassword-1');
 const password = document.querySelector('#password');
+const password1 = document.querySelector('#confirm-password');
 
 togglePassword.addEventListener('click', function () {
     // Toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
+
+    // Toggle the icon (e.g., eye vs eye-slash)
+    this.classList.toggle('fa-eye-slash');
+});
+
+togglePassword1.addEventListener('click', function () {
+    // Toggle the type attribute
+        const type = password1.getAttribute('type') === 'password' ? 'text' : 'password';
+    password1.setAttribute('type', type);
 
     // Toggle the icon (e.g., eye vs eye-slash)
     this.classList.toggle('fa-eye-slash');
